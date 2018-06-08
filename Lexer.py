c68e9472202ca6bf7e0e81b4e372dc46dd822412
FrancoARossi <rossifrancoa@gmail.com>
2018-06-08 15:12:56 -0300

    Automatas de Palabras Reservadas
    Ahí estan los atuomatas para cada una de las palabras reservadas. No estoy 100% seguro de que esten bien.

--- Lexer.py
+++ Lexer.py
@@ -4,14 +4,86 @@ def lexer(cadena):
     reservadas = ["int", "float", "if", "for", "while"]
     opLog = [":=", "<", ">", ">=", "<=", "!=", "=="]
     opMat = ["+", "*", "-", "/"]
-    simbolos = ["(", ")", "{", "}", ",", ";"]
+    simbolos = ["(", ")", "{", "}", ",", ";"]         
 
+    #Apartir de aca se aplicarian los automatas 
 
-    
-    acum = ""
-    for i in range(len (cadena)):	
-        print(i,cadena[i])
-        #if caracter.isdigit():
-       # acum = acum + caracter
-            
-lexer('hola');
+def re1 (acum):
+    s = 0
+    for c in acum:
+        if s == 0 and c == 'i':
+            s = 1
+        elif s == 1 and c == 'n':
+            s = 2
+        elif s == 2 and c == 't':
+            s = 3
+            break
+        else:
+            s = -1
+            break
+    return s == 3
+
+def re2 (acum):
+    s = 0
+    for c in acum:
+        if s == 0 and c == 'f':
+            s = 1
+        elif s == 1 and 'l':
+            s = 2
+        elif s == 2 and 'o':
+            s = 3
+        elif s == 3 and  'a':
+            s = 4
+        elif s == 4 and 't':
+            s = 5
+            break
+        else:
+            s = -1
+            break
+    return s == 5
+
+def re3 (acum):
+    s = 0
+    for c in acum:
+        if s == 0 and c == 'i':
+            s = 1
+        elif s == 1 and c == 'f':
+            s = 2
+            break
+        else:
+            s = -1
+            break
+    return s == 2
+
+def re4 (acum):
+    s = 0
+    for c in acum:
+        if s == 0 and c == 'f':
+            s = 1
+        elif s == 1 and c == 'o':
+            s = 2
+        elif s == 2 and c == 'r':
+            s = 3
+            break
+        else:
+            s = -1
+            break
+    return s == 3
+def re5 (acum):
+    s = 0
+    for c in acum:
+        if s == 0 and c == 'w':
+            s = 1
+        elif s == 1 and c == 'h':
+            s = 2
+        elif s == 2 and c == 'i':
+            s = 3
+        elif s == 3 and c == 'l':
+            s = 4
+        elif s == 4 and c == 'e':
+            s = 5
+            break
+        else:
+            s = -1
+            break
+    return s == 5
