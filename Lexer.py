e11eec430686373b9fecc45f6517b9d70fd9eec1
FrancoARossi <rossifrancoa@gmail.com>
2018-06-08 22:51:07 -0300

    Estructura inicial
    Diganme que opinan de esta estructura para el programa.

--- Lexer.py
+++ Lexer.py
@@ -1,31 +1,49 @@
 def lexer(cadena):
    
     tokens = []
-    reservadas = ["int", "float", "if", "for", "while"]
-    opLog = [":=", "<", ">", ">=", "<=", "!=", "=="]
-    opMat = ["+", "*", "-", "/"]
-    simbolos = ["(", ")", "{", "}", ",", ";"]         
+    var = (False, 0)
+    cadena = cadena + "¬"
 
-    #Apartir de aca se aplicarian los automatas 
+    while cadena[var[1]] != "¬":
+        var = re1(cadena, var[1])
+        if var[0]:
+            tokens.append(("<int>", "int"))
+        var = re2(cadena, var[1])
+        if var[0]:
+            tokens.append(("<float>", "float"))
+        var = re3(cadena, var[1])
+        if var[0]:
+            tokens.append(("<if>", "if"))
+        var = re4(cadena, var[1])
+        if var[0]:
+            tokens.append(("<for>", "for"))
+        var = re5(cadena, var[1])
+        if var[0]:
+            tokens.append(("<while>", "while"))
+    print(tokens)
 
-def re1 (acum):
+def re1 (cadena, pos):
     s = 0
-    for c in acum:
+    for i in range(pos ,len(cadena)):
+        c = cadena[i]
         if s == 0 and c == 'i':
             s = 1
         elif s == 1 and c == 'n':
             s = 2
         elif s == 2 and c == 't':
             s = 3
+            i+=1
             break
         else:
+            i-=s
             s = -1
             break
-    return s == 3
+    return (s == 3, i)
 
-def re2 (acum):
+def re2 (cadena, pos):
     s = 0
-    for c in acum:
+    for i in range(pos, len(cadena)):
+        c = cadena[i]
         if s == 0 and c == 'f':
             s = 1
         elif s == 1 and 'l':
@@ -36,42 +54,52 @@ def re2 (acum):
             s = 4
         elif s == 4 and 't':
             s = 5
+            i+=1
             break
         else:
+            i-=s
             s = -1
             break
-    return s == 5
+    return (s == 5, i)
 
-def re3 (acum):
+def re3 (cadena, pos):
     s = 0
-    for c in acum:
+    for i in range(pos, len(cadena)):
+        c = cadena[i]
         if s == 0 and c == 'i':
             s = 1
         elif s == 1 and c == 'f':
             s = 2
+            i+=1
             break
         else:
+            i-=s
             s = -1
             break
-    return s == 2
+    return (s == 2, i)
 
-def re4 (acum):
+def re4 (cadena, pos):
     s = 0
-    for c in acum:
+    for i in range(pos, len(cadena)):
+        c = cadena[i]
         if s == 0 and c == 'f':
             s = 1
         elif s == 1 and c == 'o':
             s = 2
         elif s == 2 and c == 'r':
             s = 3
+            i+=1
             break
         else:
+            i-=s
             s = -1
             break
-    return s == 3
-def re5 (acum):
+    return (s == 3, i)
+
+def re5 (cadena, pos):
     s = 0
-    for c in acum:
+    for i in range(pos, len(cadena)):
+        c = cadena[i]
         if s == 0 and c == 'w':
             s = 1
         elif s == 1 and c == 'h':
@@ -82,8 +110,12 @@ def re5 (acum):
             s = 4
         elif s == 4 and c == 'e':
             s = 5
+            i+=1
             break
         else:
+            i-=s
             s = -1
             break
-    return s == 5
+    return (s == 5, i)
+
+lexer("intfloatifforwhile")
