4f2e9dc3c2d0e2ff9feea0ffea1529b5af248a09
FrancoARossi <rossifrancoa@gmail.com>
2018-06-09 18:37:07 -0300

    Automatas varios
    Estos junto con los de operadores logicos que hizo Ivan, serian todos los automatas necesarios para la gramatica.

--- Lexer.py
+++ Lexer.py
@@ -2,24 +2,24 @@ def lexer(cadena):
    
     tokens = []
 
-def re1 (tokens, palabra):
+def a_re1 (tokens, acu):
     s = 0
-    for c in palabra:
+    for c in acu:
         if s == 0 and c == 'i':
             s = 1
         elif s == 1 and c == 'n':
             s = 2
         elif s == 2 and c == 't':
             s = 3
-            tokens.append(("<int>", "int"))
+            tokens.append(("<" + acu + ">", acu))
         else:
             s = -1
             break
     return (s == 3, tokens)
 
-def re2 (tokens, palabra):
+def a_re2 (tokens, acu):
     s = 0
-    for c in palabra:
+    for c in acu:
         if s == 0 and c == 'f':
             s = 1
         elif s == 1 and c == 'l':
@@ -30,28 +30,28 @@ def re2 (tokens, palabra):
             s = 4
         elif s == 4 and c == 't':
             s = 5
-            tokens.append(("<float>", "float"))
+            tokens.append(("<" + acu + ">", acu))
         else:
             s = -1
             break
     return (s == 5, tokens)
 
-def re3 (tokens, palabra):
+def a_re3 (tokens, acu):
     s = 0
-    for c in palabra:
+    for c in acu:
         if s == 0 and c == 'i':
             s = 1
         elif s == 1 and c == 'f':
             s = 2
-            tokens.append(("<if>", "if"))
+            tokens.append(("<" + acu + ">", acu))
         else:
             s = -1
             break
     return (s == 2, tokens)
 
-def re4 (tokens, palabra):
+def a_re4 (tokens, acu):
     s = 0
-    for c in palabra:
+    for c in acu:
         if s == 0 and c == 'e':
             s = 1
         elif s == 1 and c == 'l':
@@ -60,30 +60,30 @@ def re4 (tokens, palabra):
             s = 3
         elif s == 3 and c == 'e':
             s = 4
-            tokens.append(("<else>", "else"))
+            tokens.append(("<" + acu + ">", acu))
         else:
             s = -1
             break
     return (s == 4, tokens)
 
-def re5 (palabra):
+def a_re5 (tokens, acu):
     s = 0
-    for c in palabra:
+    for c in acu:
         if s == 0 and c == 'f':
             s = 1
         elif s == 1 and c == 'o':
             s = 2
         elif s == 2 and c == 'r':
             s = 3
-            tokens.append(("<for>", "for"))
+            tokens.append(("<" + acu + ">", acu))
         else:
             s = -1
             break
     return (s == 3, tokens)
 
-def re6 (palabra):
+def a_re6 (tokens, acu):
     s = 0
-    for c in palabra:
+    for c in acu:
         if s == 0 and c == 'w':
             s = 1
         elif s == 1 and c == 'h':
@@ -94,11 +94,114 @@ def re6 (palabra):
             s = 4
         elif s == 4 and c == 'e':
             s = 5
-            tokens.append(("<while>", "while"))
+            tokens.append(("<" + acu + ">", acu))
         else:
             s = -1
             break
     return (s == 5, tokens)
 
+def a_ParOpen (tokens, acu):
+    s = 0
+    for c in acu:
+        if c == "("
+            s = 1
+            tokens.append(("<ParOpen>", acu))
+        else:
+            s = -1
+    return (s == 1, tokens)
+
+def a_ParClose (tokens, acu):
+    s = 0
+    for c in acu:
+        if c == ")"
+            s = 1
+            tokens.append(("<ParClose>", acu))
+        else:
+            s = -1
+    return (s == 1, tokens)
+
+def a_BraOpen (tokens, acu):
+    s = 0
+    for c in acu:
+        if c == "{"
+            s = 1
+            tokens.append(("<BraOpen>", acu))
+        else:
+            s = -1
+    return (s == 1, tokens)
+
+def a_BraClose (tokens, acu):
+    s = 0
+    for c in acu:
+        if c == "}"
+            s = 1
+            tokens.append(("<BraClose>", acum))
+        else:
+            s = -1
+    return (s == 1, tokens)
+
+def a_Coma (tokens, acu):
+    s = 0
+    for c in acu:
+        if c == ","
+            s = 1
+            tokens.append(("<Coma>", acum))
+        else:
+            s = -1
+    return (s == 1, tokens)
+
+def a_PointComa (tokens, acu):
+    s = 0
+    for c in acu:
+        if c == ";"
+            s = 1
+            tokens.append(("<PointComa>", acum))
+        else:
+            s = -1
+    return (s == 1, tokens)
+
+def a_Sum (tokens, acu):
+    s = 0
+    for c in acu:
+        if c == "+"
+            s = 1
+            tokens.append(("<Sum>", acum))
+        else:
+            s = -1
+    return (s == 1, tokens)
+
+def a_Minus (tokens, acu):
+    s = 0
+    for c in acu:
+        if c == "-"
+            s = 1
+            tokens.append(("<Minus>", acum))
+        else:
+            s = -1
+    return (s == 1, tokens)
+
+def a_Product (tokens, acu):
+    s = 0
+    for c in acu:
+        if c == "*"
+            s = 1
+            tokens.append(("<Product>", acum))
+        else:
+            s = -1
+    return (s == 1, tokens)
+
+def a_Division (tokens, acu):
+    s = 0
+    for c in acu:
+        if c == "/"
+            s = 1
+            tokens.append(("<Division>", acum))
+        else:
+            s = -1
+    return (s == 1, tokens)
+
 # Al finalizar el lexer deberia aceptar esta cadena
-lexer("int miFuncion(float a,int b){ for(c:=9, x == y) a := 2+2;}")
+lexer("int miFuncion(float a,int b){ for(c:=9, x <= y) a := 2+2;}")
+
+# Habria que mandar un mensaje de error en este caso
+lexer("1 <( 2")
