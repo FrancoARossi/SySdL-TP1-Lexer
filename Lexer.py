66ba10bc609a4fea283d902dc2ebb04bf98eb293
Ivan <im.moralesivan@gmail.com>
2018-06-10 19:48:28 -0300

    YA FUNCIONA
    Falta probarlo mas, y borrar sentencias de debugging

--- Lexer.py
+++ Lexer.py
@@ -5,14 +5,19 @@ def lexer(cadena):
 	OpMat = ["+", "*", "-", "/"]
 	simbolos = ["(", ")", "{", "}", ",", ";"]
 	tokens = []
+	i=0
+	cadena = cadena + ' '
 		
-	for i in len(cadena):
+	while i<len(cadena):
 		acu = ""
-		if cadena[i].isAlpha():
+		print("Entro while general con '",acu,"'")
+		print("i:",i)
+		if cadena[i].isalpha():
 			acu = acu + cadena[i]
-			start=i
+			print("Entro isalpha con '",acu,"'")			
+			start=i+1
 			for x in range(start,len(cadena)):
-				if cadena[x].isAlpha():
+				if cadena[x].isalpha():
 					acu = acu + cadena[x]
 				else:
 					i = x
@@ -27,21 +32,31 @@ def lexer(cadena):
 						if not pertenece:
 							pertenece = a_re5(tokens,acu)
 							if not pertenece:
-								tokens.append(("<ID>",acu))
-		elif cadena[i].isDigit():
+								pertenece = a_re6(tokens,acu)
+								if not pertenece:
+									pertenece = a_ID(tokens,acu)
+			print("Salio isalpha con '",acu,"'")
+		elif cadena[i].isdigit():
 			acu = acu + cadena[i]
+			print("Entro isdigit con '",acu,"'")
+			start=i+1
 			for x in range(start,len(cadena)):
-				if cadena[x].isDigit():
+				if cadena[x].isdigit():
 					acu = acu + cadena[x]
 				else:
 					i = x
+					print("Hace esto alguna vez!?")
 					break
-			tokens.append(("<Num>",acu))               # Aca ya sabemos que es un numero, asi que no llamamos al automata de num... ??
-		elif cadena[i].isSpace():
-			pass            # No debe hacer nada, que siga con el siguiente caracter
+			tokens.append(("<Num>",acu))               # Aca ya sabemos que es un numero, asi que no llamamos al automata de num... RIGHT??
+			print("Salio isdigit con '",acu,"'")
+		elif cadena[i].isspace():
+			i+=1         # No debe hacer nada, que siga con el siguiente caracter
 		else:
 			acu = acu + cadena [i]
-			if acu in (OpMat or simbolos):
+			print("Entro simbolos con'",acu,"'")
+			if (acu in OpMat) or (acu in simbolos):
+				i+=1
+				print("Estuvo en una de las listas")
 				pertenece = a_ParOpen(tokens,acu)
 				if not pertenece:
 					pertenece = a_ParClose(tokens,acu)
@@ -62,9 +77,12 @@ def lexer(cadena):
 												if not pertenece:
 													pertenece = a_Divide(tokens,acu)
 			else:
+				print("No estuvo, fue por el else")
 				i+=1
-				if not cadena[i].isAlpha() and not cadena[i].isDigit() and not cadena[i].isSpace:
+				if not cadena[i].isalpha() and not cadena[i].isdigit() and not cadena[i].isspace():
 					acu = acu + cadena[i]
+					print("Agrego '",cadena[i],"' , acu quedo en '",acu,"'")
+					i+=1
 				pertenece = a_OpRel1(tokens,acu)
 				if not pertenece:
 					pertenece = a_OpRel2(tokens,acu)
@@ -78,16 +96,31 @@ def lexer(cadena):
 									pertenece = a_OpRel6(tokens,acu)
 									if not pertenece:
 										pertenece = a_OpRel7(tokens,acu)
+			print("Salio simbolos con'",acu,"'")
+		print("Salio while general con '",acu,"'")
 	print(tokens)
-	time.sleep(5)
+	print("The end.")
 
+def a_ID(tokens, acu):
+	s=0
+	for c in acu:
+		if s==0 and c.isalpha():
+			s=1
+		elif s==1 and c.isalpha():
+			s=1
+		else:
+			s=-1
+			break
+	if s==1:
+		tokens.append(("<ID>",acu))
+	return s==1
 
 def a_OpRel1(tokens, acu):
 	s=0;
-	for c in x:
+	for c in acu:
 		if s==0 and c==':':
 			s=1
-		elif S==1 and c=='=':
+		elif s==1 and c=='=':
 			s=2
 		else:
 			s=-1
@@ -98,7 +131,7 @@ def a_OpRel1(tokens, acu):
 
 def a_OpRel2(tokens, acu):
 	s=0;
-	for c in x:
+	for c in acu:
 		if s==0 and c=='<':
 			s=1
 		else:
@@ -110,7 +143,7 @@ def a_OpRel2(tokens, acu):
 
 def a_OpRel3(tokens, acu):
 	s=0;
-	for c in x:
+	for c in acu:
 		if s==0 and c=='>':
 			s=1
 		else:
@@ -123,7 +156,7 @@ def a_OpRel3(tokens, acu):
 
 def a_OpRel4(tokens, acu):
 	s=0;
-	for c in x:
+	for c in acu:
 		if s==0 and c=='<':
 			s=1
 		elif s==1 and c=='=':
@@ -137,7 +170,7 @@ def a_OpRel4(tokens, acu):
 
 def a_OpRel5(tokens, acu):
 	s=0;
-	for c in x:
+	for c in acu:
 		if s==0 and c=='>':
 			s=1
 		elif s==1 and c=='=':
@@ -151,7 +184,7 @@ def a_OpRel5(tokens, acu):
 	
 def a_OpRel6(tokens, acu):
 	s=0;
-	for c in x:
+	for c in acu:
 		if s==0 and c=='!':
 			s=1
 		elif s==1 and c=='=':
@@ -165,7 +198,7 @@ def a_OpRel6(tokens, acu):
 
 def a_OpRel7(tokens, acu):
 	s=0;
-	for c in x:
+	for c in acu:
 		if s==0 and c=='=':
 			s=1
 		elif s==1 and c=='=':
@@ -289,7 +322,7 @@ def a_ParOpen (tokens, acu):
         else:
             s = -1
     if s == 1:
-        tokens.append(("<" + acu + ">", acu))
+        tokens.append(("<ParOpen>", acu))
     return (s == 1)
 
 def a_ParClose (tokens, acu):
@@ -300,7 +333,7 @@ def a_ParClose (tokens, acu):
         else:
             s = -1
     if s == 1:
-        tokens.append(("<" + acu + ">", acu))
+        tokens.append(("<ParClose>", acu))
     return (s == 1)
 
 def a_BraOpen (tokens, acu):
@@ -311,7 +344,7 @@ def a_BraOpen (tokens, acu):
         else:
             s = -1
     if s == 1:
-        tokens.append(("<" + acu + ">", acu))
+        tokens.append(("<BraOpen>", acu))
     return (s == 1)
 
 def a_BraClose (tokens, acu):
@@ -322,7 +355,7 @@ def a_BraClose (tokens, acu):
         else:
             s = -1
     if s == 1:
-        tokens.append(("<" + acu + ">", acu))
+        tokens.append(("<BraClose>", acu))
     return (s == 1)
 
 def a_Coma (tokens, acu):
@@ -333,7 +366,7 @@ def a_Coma (tokens, acu):
         else:
             s = -1
     if s == 1:
-        tokens.append(("<" + acu + ">", acu))
+        tokens.append(("<Comma>", acu))
     return (s == 1)
 
 def a_PointComa (tokens, acu):
@@ -344,7 +377,7 @@ def a_PointComa (tokens, acu):
         else:
             s = -1
     if s == 1:
-        tokens.append(("<" + acu + ">", acu))
+        tokens.append(("<SemiColon>", acu))
     return (s == 1)
 
 def a_Sum (tokens, acu):
@@ -393,6 +426,7 @@ def a_Division (tokens, acu):
 
 # Al finalizar el lexer deberia aceptar esta cadena
 lexer("int miFuncion(float a,int b){ for(c:=9, x <= y) a := 2+2;}")
+# lexer(" while 1234")
 
 # Habria que mandar un mensaje de error en este caso
 # lexer("1 <( 2")
