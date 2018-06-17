4daa05acf46095c2fa33b6273e33395823a98464
ivi982010 <im.moralesivan@gmail.com>
2018-06-17 18:16:19 -0300

    Preparando para entrega
    Uniformando la escritura de sentencias, ordenando y mejorando comentarios en general

--- Lexer.py
+++ Lexer.py
@@ -1,6 +1,6 @@
 SimbolosDeLaGramatica = [':=', '<', '>', '>=', '<=', '!=', '==', '(', ')', '{', '}', ',', '+', '*', '-', '/']
 
-def lexer(src):
+def lexer(src) :
 	tokens = []
 	src = src + ' '
 	i = 0
@@ -11,409 +11,424 @@ def lexer(src):
 		c = src[i]
 		word = src[start:i+1]
 		if state == 0 :
-			if c.isspace():
+			if c.isspace() :
 				i += 1
 				state = 0
-			else:
+			else :
 				start = i
 				state = 1
 		elif state == 1 :
 			if (es_aceptado(word) and not c.isspace()) :
 				i += 1
 				state = 1
-			else:
+			else :
 				i -= 1
 				state = 2
 		elif state == 2 :
 			candidatos = evaluar(word)
-			if (len(candidatos) == 0):
+			if (len(candidatos) == 0) :
 				print("Error, lista de candidatos vacia.")
 				break
 			tokentype = candidatos[0]
 			tokens.append((tokentype, word))
 			i += 1
 			state = 0
+	
 	return tokens
 
-# Esta funcion busca candidatos para la palabra actual
+# Esta funcion devuelve True si la palabra es aceptada por al menos un automata
 
-def es_aceptado(word):
+def es_aceptado(word) :
 	candidatos = []
 	for (token, afd) in TT :
 		if afd(word) :
 			candidatos.append(token)		
 	if len(candidatos) > 0 :
 		return True
-	else:
+	else :
 		return False
 
-# Esta funcion evalua los candidatos encontrados
+# Esta funcion evalua la palabra en todos los automatas y devuelva la lista de de candidatos
+# a token hasta el momento
 
-def evaluar(word):
+def evaluar(word) :
 	candidatos = []
 	for (token,afd) in TT :
 		if afd(word) :
 			candidatos.append(token)
 	return candidatos
 
-# Automatas de tokens
+## Automatas de error de mayor prioridad
 
-def a_ID(word):
-	s = 0
-	for c in word:
-		if s == 0 and c.isalpha():
-			s = 1
-		elif s == 1 and c.isalpha():
-			s = 1
-		else:
-			s = -1
-			break
-	return (s == 1)
-
-def a_SimbUnico(word):
-	s = 0
-	for c in word:
-		if s == 0 and (c == '<' or c == '>' or c == ':' or c == '!' or c == '='):
-			s = 1
-		else:
-			s = -1
-			break
-	return (s == 1)
-
-def a_Num(word):
-	s = 0
-	for c in word:
-		if s == 0 and c.isdigit():
-			s = 1
-		elif s == 1 and c.isdigit():
-			s = 1
-		else:
-			s = -1
-			break
-	return (s == 1)
-
-def a_OpRel1(word):
-	s = 0
-	for c in word:
-		if s == 0 and c == ':':
-			s = 1
-		elif s==1 and c == '=':
-			s = 2
-		else:
-			s = -1
-			break
-	return (s == 2)
-
-def a_OpRel2(word):
-	s = 0
-	for c in word:
-		if s == 0 and c == '<':
-			s = 1
-		else:
-			s = -1
-			break
-	return (s == 1)
-
-def a_OpRel3(word):
-	s= 0
-	for c in word:
-		if s == 0 and c == '>':
-			s = 1
-		else:
-			s = -1
-			break
-	return (s == 1)
-
-def a_OpRel4(word):
-	s = 0
-	for c in word:
-		if s == 0 and c == '<':
-			s = 1
-		elif s==1 and c == '=':
-			s = 2
-		else:
-			s = -1
-			break
-	return (s == 2)
-
-def a_OpRel5(word):
-	s = 0
-	for c in word:
-		if s == 0 and c == '>':
-			s = 1
-		elif s == 1 and c == '=':
-			s = 2
-		else:
-			s = -1
-			break
-	return (s == 2)
-	
-def a_OpRel6(word):
-	s = 0
-	for c in word:
-		if s == 0 and c == '!':
-			s = 1
-		elif s == 1 and c == '=':
-			s = 2
-		else:
-			s = -1
-			break
-	return (s == 2)
-
-def a_OpRel7(word):
-	s = 0
-	for c in word:
-		if s == 0 and c == '=':
-			s = 1
-		elif s == 1 and c == '=':
-			s = 2
-		else:
-			s = -1
-			break
-	return (s == 2)
-
-def a_re1 (word):
-	s = 0
-	for c in word:
-		if s == 0 and c == 'i':
-			s = 1
-		elif s == 1 and c == 'n':
-			s = 2
-		elif s == 2 and c == 't':
-			s = 3
-		else:
-			s = -1
-			break
-	return (s == 3)
-
-def a_re2 (word):
-	s = 0
-	for c in word:
-		if s == 0 and c == 'f':
-			s = 1
-		elif s == 1 and c == 'l':
-			s = 2
-		elif s == 2 and c == 'o':
-			s = 3
-		elif s == 3 and c == 'a':
-			s = 4
-		elif s == 4 and c == 't':
-			s = 5
-		else:
-			s = -1
-			break
-	return (s == 5)
-
-def a_re3 (word):
-	s = 0
-	for c in word:
-		if s == 0 and c == 'i':
-			s = 1
-		elif s == 1 and c == 'f':
-			s = 2
-		else:
-			s = -1
-			break
-	return (s == 2)
-
-def a_re4 (word):
-	s = 0
-	for c in word:
-		if s == 0 and c == 'e':
-			s = 1
-		elif s == 1 and c == 'l':
-			s = 2
-		elif s == 2 and c == 's':
-			s = 3
-		elif s == 3 and c == 'e':
-			s = 4
-		else:
-			s = -1
-			break
-	return (s == 4)
-
-def a_re5 (word):
-	s = 0
-	for c in word:
-		if s == 0 and c == 'f':
-			s = 1
-		elif s == 1 and c == 'o':
-			s = 2
-		elif s == 2 and c == 'r':
-			s = 3
-		else:
-			s = -1
-			break
-	return (s == 3)
-
-def a_re6 (word):
-	s = 0
-	for c in word:
-		if s == 0 and c == 'w':
-			s = 1
-		elif s == 1 and c == 'h':
-			s = 2
-		elif s == 2 and c == 'i':
-			s = 3
-		elif s == 3 and c == 'l':
-			s = 4
-		elif s == 4 and c == 'e':
-			s = 5
-		else:
-			s = -1
-			break
-	return (s == 5)
-
-def a_ParOpen (word):
-	s = 0
-	for c in word:
-		if s == 0 and c == '(':
-			s = 1
-		else:
-			s = -1
-			break
-	return (s == 1)
-
-def a_ParClose (word):
-	s = 0
-	for c in word:
-		if s == 0 and c == ')':
-			s = 1
-		else:
-			s = -1
-			break
-	return (s == 1)
-
-def a_BraOpen (word):
-	s = 0
-	for c in word:
-		if s == 0 and c == '{':
-			s = 1
-		else:
-			s = -1
-			break
-	return (s == 1)
-
-def a_BraClose (word):
-	s = 0
-	for c in word:
-		if s == 0 and c == '}':
-			s = 1
-		else:
-			s = -1
-			break
-	return (s == 1)
-
-def a_Comma (word):
-	s = 0
-	for c in word:
-		if s == 0 and c == ',':
-			s = 1
-		else:
-			s = -1
-			break
-	return (s == 1)
-
-def a_SemiColon (word):
-	s = 0
-	for c in word:
-		if s == 0 and c == ';':
-			s = 1
-		else:
-			s = -1
-			break
-	return (s == 1)
-
-def a_Sum (word):
-	s = 0
-	for c in word:
-		if s == 0 and c == '+':
-			s = 1
-		else:
-			s = -1
-			break
-	return (s == 1)
-
-def a_Minus (word):
-	s = 0
-	for c in word:
-		if s == 0 and c == '-':
-			s = 1
-		else:
-			s = -1
-			break
-	return (s == 1)
-
-def a_Product (word):
-	s = 0
-	for c in word:
-		if s == 0 and c == '*':
-			s = 1
-		else:
-			s = -1
-			break
-	return (s == 1)
-
-def a_Division (word):
-	s = 0
-	for c in word:
-		if s == 0 and c == '/':
-			s = 1
-		else:
-			s = -1
-			break
-	return (s == 1)
-	
-def a_ErrorIDNum (word):
+def a_ErrorIDNum(word) :
 	s = 0
 	c = word[0]
 	if c.isalpha() :
 		s = 1
 		for c in word :
-			if c.isalpha():
+			if c.isalpha() :
 				s = 1
-			elif s == 1 and c.isdigit():
+			elif (s == 1 and c.isdigit()) :
 				s = 2
 				break
 	elif c.isdigit() :
 		s = 1
 		for c in word :
-			if c.isdigit():
+			if c.isdigit() :
 				s = 1
-			elif s == 1 and c.isalpha():
+			elif (s == 1 and c.isalpha()) :
 				s = 2
 				break
 	return (s == 2)
 
-# Aca se usan 3 estados para hacer el automata recursivo que detecte multiples simbolos
-def a_ErrorOpMat (word):
+# Aqui se usan tres estados ya que si usáramos dos, tomaria prioridad como aceptado
+# antes que los automatas de operadores matemáticos.
+def a_ErrorOpMat(word) :
 	s = 0
-	for c in word:
-		if s == 0 and (c == "+" or c == "-" or c == "/" or c == "*"):
+	for c in word :
+		if (s == 0 and (c == "+" or c == "-" or c == "/" or c == "*")) :
 			s = 1
-		elif s == 1 and (c == "+" or c == "-" or c == "/" or c == "*"):
+		elif (s == 1 and (c == "+" or c == "-" or c == "/" or c == "*")) :
 			s = 2
-		elif s == 2 and (c == "+" or c == "-" or c == "/" or c == "*"):
-			pass
-		else:
+		elif (s == 2 and (c == "+" or c == "-" or c == "/" or c == "*")) :
+			s = 2
+		else :
 			s = -1
 			break
 	return (s == 2)
 
-def a_ErrorSimbInvalido (word):
+## Automatas de tokens
+
+# Automatas de palabras reservadas
+
+def a_re1(word) :
 	s = 0
-	for c in word:
-		if s == 0 and (not c in SimbolosDeLaGramatica and not c.isalpha() and not c.isdigit()):
+	for c in word :
+		if (s == 0 and c == 'i') :
 			s = 1
-		elif s == 1 and (not c in SimbolosDeLaGramatica and not c.isalpha() and not c.isdigit()):
-			pass
-		else:
+		elif (s == 1 and c == 'n') :
+			s = 2
+		elif (s == 2 and c == 't') :
+			s = 3
+		else :
+			s = -1
+			break
+	return (s == 3)
+
+def a_re2(word) :
+	s = 0
+	for c in word :
+		if (s == 0 and c == 'f') :
+			s = 1
+		elif (s == 1 and c == 'l') :
+			s = 2
+		elif (s == 2 and c == 'o') :
+			s = 3
+		elif (s == 3 and c == 'a') :
+			s = 4
+		elif (s == 4 and c == 't') :
+			s = 5
+		else :
+			s = -1
+			break
+	return (s == 5)
+
+def a_re3(word) :
+	s = 0
+	for c in word :
+		if (s == 0 and c == 'i') :
+			s = 1
+		elif (s == 1 and c == 'f') :
+			s = 2
+		else :
+			s = -1
+			break
+	return (s == 2)
+
+def a_re4(word) :
+	s = 0
+	for c in word :
+		if (s == 0 and c == 'e') :
+			s = 1
+		elif (s == 1 and c == 'l') :
+			s = 2
+		elif (s == 2 and c == 's') :
+			s = 3
+		elif (s == 3 and c == 'e') :
+			s = 4
+		else :
+			s = -1
+			break
+	return (s == 4)
+
+def a_re5(word) :
+	s = 0
+	for c in word :
+		if (s == 0 and c == 'f') :
+			s = 1
+		elif (s == 1 and c == 'o') :
+			s = 2
+		elif (s == 2 and c == 'r') :
+			s = 3
+		else :
+			s = -1
+			break
+	return (s == 3)
+
+def a_re6(word) :
+	s = 0
+	for c in word :
+		if (s == 0 and c == 'w') :
+			s = 1
+		elif (s == 1 and c == 'h') :
+			s = 2
+		elif (s == 2 and c == 'i') :
+			s = 3
+		elif (s == 3 and c == 'l') :
+			s = 4
+		elif (s == 4 and c == 'e') :
+			s = 5
+		else :
+			s = -1
+			break
+	return (s == 5)
+
+def a_ID(word) :
+	s = 0
+	for c in word :
+		if (s == 0 and c.isalpha()) :
+			s = 1
+		elif (s == 1 and c.isalpha()) :
+			s = 1
+		else :
 			s = -1
 			break
 	return (s == 1)
 
-# La lista de Tipos de Tokens esta ordenada por prioridad,
-# ya que el candidato utilizado sera el primero de la lista.
+def a_Num(word) :
+	s = 0
+	for c in word :
+		if (s == 0 and c.isdigit()) :
+			s = 1
+		elif (s == 1 and c.isdigit()) :
+			s = 1
+		else :
+			s = -1
+			break
+	return (s == 1)
+
+# Automatas de operadores relacionales
+
+def a_OpRel1(word) :
+	s = 0
+	for c in word :
+		if (s == 0 and c == ':') :
+			s = 1
+		elif (s == 1 and c == '=') :
+			s = 2
+		else :
+			s = -1
+			break
+	return (s == 2)
+
+def a_OpRel2(word) :
+	s = 0
+	for c in word :
+		if (s == 0 and c == '<') :
+			s = 1
+		else :
+			s = -1
+			break
+	return (s == 1)
+
+def a_OpRel3(word) :
+	s= 0
+	for c in word :
+		if (s == 0 and c == '>') :
+			s = 1
+		else :
+			s = -1
+			break
+	return (s == 1)
+
+def a_OpRel4(word) :
+	s = 0
+	for c in word :
+		if (s == 0 and c == '<') :
+			s = 1
+		elif (s == 1 and c == '=') :
+			s = 2
+		else :
+			s = -1
+			break
+	return (s == 2)
+
+def a_OpRel5(word) :
+	s = 0
+	for c in word :
+		if (s == 0 and c == '>') :
+			s = 1
+		elif (s == 1 and c == '=') :
+			s = 2
+		else :
+			s = -1
+			break
+	return (s == 2)
+	
+def a_OpRel6(word) :
+	s = 0
+	for c in word :
+		if (s == 0 and c == '!') :
+			s = 1
+		elif (s == 1 and c == '=') :
+			s = 2
+		else :
+			s = -1
+			break
+	return (s == 2)
+
+def a_OpRel7(word) :
+	s = 0
+	for c in word :
+		if (s == 0 and c == '=') :
+			s = 1
+		elif (s == 1 and c == '=') :
+			s = 2
+		else :
+			s = -1
+			break
+	return (s == 2)
+
+# Automatas de simbolos
+
+def a_ParOpen(word) :
+	s = 0
+	for c in word :
+		if (s == 0 and c == '(') :
+			s = 1
+		else :
+			s = -1
+			break
+	return (s == 1)
+
+def a_ParClose(word) :
+	s = 0
+	for c in word :
+		if (s == 0 and c == ')') :
+			s = 1
+		else :
+			s = -1
+			break
+	return (s == 1)
+
+def a_BraOpen(word) :
+	s = 0
+	for c in word :
+		if (s == 0 and c == '{') :
+			s = 1
+		else :
+			s = -1
+			break
+	return (s == 1)
+
+def a_BraClose(word) :
+	s = 0
+	for c in word :
+		if (s == 0 and c == '}') :
+			s = 1
+		else :
+			s = -1
+			break
+	return (s == 1)
+
+def a_Comma(word) :
+	s = 0
+	for c in word :
+		if (s == 0 and c == ',') :
+			s = 1
+		else :
+			s = -1
+			break
+	return (s == 1)
+
+def a_SemiColon(word) :
+	s = 0
+	for c in word :
+		if (s == 0 and c == ';') :
+			s = 1
+		else :
+			s = -1
+			break
+	return (s == 1)
+
+def a_Sum(word) :
+	s = 0
+	for c in word :
+		if (s == 0 and c == '+') :
+			s = 1
+		else :
+			s = -1
+			break
+	return (s == 1)
+
+def a_Minus(word) :
+	s = 0
+	for c in word :
+		if (s == 0 and c == '-') :
+			s = 1
+		else :
+			s = -1
+			break
+	return (s == 1)
+
+def a_Product(word) :
+	s = 0
+	for c in word :
+		if (s == 0 and c == '*') :
+			s = 1
+		else :
+			s = -1
+			break
+	return (s == 1)
+
+def a_Division(word) :
+	s = 0
+	for c in word :
+		if (s == 0 and c == '/') :
+			s = 1
+		else :
+			s = -1
+			break
+	return (s == 1)
+
+## Automatas de error de menor prioridad
+
+# Este automata fue necesario debido a que sino la lista de candidatos quedaba vacia
+# al momento de evaluar el primer caracter de los simbolos dobles.
+def a_SimbUnico(word) :
+	s = 0
+	for c in word :
+		if (s == 0 and (c == '<' or c == '>' or c == ':' or c == '!' or c == '=')) :
+			s = 1
+		else :
+			s = -1
+			break
+	return (s == 1)
+
+def a_ErrorSimbInvalido(word) :
+	s = 0
+	for c in word :
+		if s == 0 and (not c in SimbolosDeLaGramatica and not c.isalpha() and not c.isdigit()) :
+			s = 1
+		elif s == 1 and (not c in SimbolosDeLaGramatica and not c.isalpha() and not c.isdigit()) :
+			s = 1
+		else :
+			s = -1
+			break
+	return (s == 1)
+
+# La lista de Tipos de Token esta ordenada por prioridad,
+# ya que el candidato que prevalecerá será el primero de esta.
 
 TT = [("errorIDNum", a_ErrorIDNum), ("errorOpMat", a_ErrorOpMat),
 	 ("Reservada", a_re1), ("Reservada", a_re2), ("Reservada", a_re3), ("Reservada",a_re4),
@@ -424,16 +439,15 @@ TT = [("errorIDNum", a_ErrorIDNum), ("errorOpMat", a_ErrorOpMat),
 	 ("BraOpen", a_BraOpen), ("BraClose", a_BraClose), ("Comma", a_Comma), ("SemiColon", a_SemiColon),
 	 ("OpMat", a_Sum), ("OpMat" ,a_Minus), ("OpMat", a_Product), ("OpMat", a_Division), ("SimbUnico" ,a_SimbUnico),
 	 ("errorSimbInvalido", a_ErrorSimbInvalido)]
-	 #("errorInvalido", a_ErrorInvalido)]
 
-# Estas srcs deben devolver un token de error
-print(lexer("a := 3 %~ ^^ ´ 2;"))
-print("")
+## Sources
+
 print(lexer("int miFuncion(float a,int b){ for(c:=9, x <= y) a := 2+2}"))
 print("")
 
-# Estas srcs deben devolver un token de error
-
+# Estas sources deben devolver un token de error
+print(lexer("a := 3 %~ ^^ ´ 2;"))
+print("")
 print(lexer("abc123"))
 print("")
 print(lexer("123abc"))
@@ -443,4 +457,3 @@ print("")
 print(lexer("a := 3 -+/** 2;"))
 print("")
 print(lexer("a := 3 % 2;"))
-# Exit Debug
