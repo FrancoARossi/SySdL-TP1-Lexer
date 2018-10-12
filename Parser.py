eace8f397c8d12f03dfaad63e331ad26e8e792f7
FrancoARossi <rossifrancoa@gmail.com>
2018-10-11 22:59:38 -0300

    Estructura principal

--- Parser.py
+++ Parser.py
@@ -1,2 +1,21 @@
 from Lexer.py import lexer
 
+def parser(TokenTypes):
+	error = False
+	# la variable t (indice del token apuntado) es global
+	t = 0
+	PN1(t)
+	if not error and finDeCadena(TokenTypes, t):
+		return True
+	else:
+		return False
+
+def PN1(pw):
+	pass
+
+def Procesar(produc, pw):
+	pass
+
+def finDeCadena(TokenTypes, t):
+	#se compara TokenTypes(len(TokenTypes)) == lista[t]
+	pass
