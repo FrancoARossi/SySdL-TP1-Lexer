82ba0eff6b736a846a3de56b6adfb62e95186190
FrancoARossi <rossifrancoa@gmail.com>
2018-10-15 21:45:16 -0300

    Avanze un poco con PN1 y agregue algunos comentarios que habria que revisar

--- Parser.py
+++ Parser.py
@@ -3,19 +3,30 @@ from Lexer.py import lexer
 def parser(TokenTypes):
 	error = False
 	# la variable t (indice del token apuntado) es global
-	t = 0
-	PN1(t)
-	if not error and finDeCadena(TokenTypes, t):
+	pw = 0
+	PN1(t, error)
+	if not error and finDeCadena(TokenTypes, pw):
 		return True
 	else:
 		return False
 
-def PN1(pw):
+def PN1(pw, error):
+	j = 1
+	# Python no tiene un ciclo del tipo Repetir-Hasta, hay que usar un while true con un if como condicion de salida
+	while True:
+		error = False
+		Procesar(produ, pw, error)
+		j += 1
+		#No me cierra esta condicion de salida... en el algoritmo pone j <= k donde k es el numero de producciones de Ni
+		# en este caso hay 1 sola produccion pero por como esta planteado el algoritmo cuando se llega al if por primera
+		# vez j ya vale 2 
+		if error and (j <= 1):
+			break
+
+
+def Procesar(produ, pw, error):
 	pass
 
-def Procesar(produc, pw):
-	pass
-
-def finDeCadena(TokenTypes, t):
+def finDeCadena(TokenTypes, pw):
 	#se compara TokenTypes(len(TokenTypes)) == lista[t]
 	pass
