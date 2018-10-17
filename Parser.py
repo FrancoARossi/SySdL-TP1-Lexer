8ca1d15ca317b1f93c444d9bdbb57bc1e0e4feae
Ivan <im.moralesivan@gmail.com>
2018-10-17 17:28:04 -0300

    Algunas dudas iniciales solucionadas

--- Parser.py
+++ Parser.py
@@ -12,20 +12,22 @@ def parser(TokenTypes):
 
 def PN1(pw, error):
 	j = 1
-	# Python no tiene un ciclo del tipo Repetir-Hasta, hay que usar un while true con un if como condicion de salida
-	while True:
+	#No me cierra esta condicion de salida... en el algoritmo pone j <= k donde k es el numero de producciones de Ni
+	# en este caso hay 1 sola produccion pero por como esta planteado el algoritmo cuando se llega al if por primera
+	# vez j ya vale 2
+
+	# esta bien que rompa ya que el procedimiento lo debe hacer solo una vez para PN1 (ya que tiene una sola produccion
+	# el simbolo inicial)
+
+	while (error and (j <= 1)):
 		error = False
 		Procesar(produ, pw, error)
 		j += 1
-		#No me cierra esta condicion de salida... en el algoritmo pone j <= k donde k es el numero de producciones de Ni
-		# en este caso hay 1 sola produccion pero por como esta planteado el algoritmo cuando se llega al if por primera
-		# vez j ya vale 2
-		if error and (j <= 1):
-			break
-# Yo creo que hay que preguntar bien por ese procedmiento de PNi que esta en la diapo
-# en ningun momento declara k ni llega como parametro, que por lo que pude entender
-# es la cantidad de producciones del VN
-# Ademas no deberia correr mientras que NO haya error Y la produccion exista? (j<k)
+
+# lo de la diapositiva no es un repetir hasta, es un while normal, ya que continua en el ciclo MIENTRAS
+# se cumpla la condicion, lo que me parece raro es que en la condicion este error si negar, por lo que
+# si llega con el valor por defecto (false) no va a entrar en el ciclo, deberiamos preguntar que onda eso.
+# Para mi deberia ser while (not(error) and (j <= 1))
 
 def Procesar(produ, pw, error):
 	pass
