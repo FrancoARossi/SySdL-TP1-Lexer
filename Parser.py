3013154fd055e109e947e461043e009763d7b1ec
Ivan <im.moralesivan@gmail.com>
2018-10-17 16:54:48 -0300

    Revisando

--- Parser.py
+++ Parser.py
@@ -19,10 +19,13 @@ def PN1(pw, error):
 		j += 1
 		#No me cierra esta condicion de salida... en el algoritmo pone j <= k donde k es el numero de producciones de Ni
 		# en este caso hay 1 sola produccion pero por como esta planteado el algoritmo cuando se llega al if por primera
-		# vez j ya vale 2 
+		# vez j ya vale 2
 		if error and (j <= 1):
 			break
-
+# Yo creo que hay que preguntar bien por ese procedmiento de PNi que esta en la diapo
+# en ningun momento declara k ni llega como parametro, que por lo que pude entender
+# es la cantidad de producciones del VN
+# Ademas no deberia correr mientras que NO haya error Y la produccion exista? (j<k)
 
 def Procesar(produ, pw, error):
 	pass
