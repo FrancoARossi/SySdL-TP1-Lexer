077b6ac82ea2f0471abda14c9256f86617e63eaa
Ivan <im.moralesivan@gmail.com>
2018-10-17 20:07:49 -0300

    Error variable global

--- Parser.py
+++ Parser.py
@@ -1,7 +1,8 @@
 from Lexer.py import lexer
 
+error = False
+
 def parser(TokenTypes):
-	error = False
 	# la variable t (indice del token apuntado) es global
 	pw = 0
 	PN1(t, error)
@@ -10,8 +11,7 @@ def parser(TokenTypes):
 	else:
 		return False
 
-def PN1(pw, error):
-	j = 1
+def PN1(pw):
 	#No me cierra esta condicion de salida... en el algoritmo pone j <= k donde k es el numero de producciones de Ni
 	# en este caso hay 1 sola produccion pero por como esta planteado el algoritmo cuando se llega al if por primera
 	# vez j ya vale 2
@@ -19,9 +19,10 @@ def PN1(pw, error):
 	# esta bien que rompa ya que el procedimiento lo debe hacer solo una vez para PN1 (ya que tiene una sola produccion
 	# el simbolo inicial)
 
+	j = 1
 	while (error and (j <= 1)):
 		error = False
-		Procesar(produ, pw, error)
+		Procesar(produ, pw)
 		j += 1
 
 # lo de la diapositiva no es un repetir hasta, es un while normal, ya que continua en el ciclo MIENTRAS
@@ -29,7 +30,7 @@ def PN1(pw, error):
 # si llega con el valor por defecto (false) no va a entrar en el ciclo, deberiamos preguntar que onda eso.
 # Para mi deberia ser while (not(error) and (j <= 1))
 
-def Procesar(produ, pw, error):
+def Procesar(produ, pw):
 	pass
 
 def finDeCadena(TokenTypes, pw):
